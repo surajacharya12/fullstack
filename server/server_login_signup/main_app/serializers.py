ed0746980eac5e7ecb0dc96dff6c96b0785fa528
surajacharya12 <surajacharya.080@kathford.edu.np>
2026-03-13 20:56:30 +0545

    image function update in backend

--- server/server_login_signup/main_app/serializers.py
+++ server/server_login_signup/main_app/serializers.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Blog
+from .models import Blog, Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    profile = ProfileSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'profile']
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -15,12 +21,14 @@ class UserSerializer(serializers.ModelSerializer):
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
+        Profile.objects.create(user=user)
         return user
 
 class BlogSerializer(serializers.ModelSerializer):
     author_name = serializers.ReadOnlyField(source='author.username')
+    author_avatar = serializers.ImageField(source='author.profile.avatar', read_only=True)
 
     class Meta:
         model = Blog
-        fields = ['id', 'title', 'content', 'topic', 'author', 'author_name', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'content', 'topic', 'image', 'author', 'author_name', 'author_avatar', 'created_at', 'updated_at']
         read_only_fields = ['author', 'created_at', 'updated_at']
--- server/server_login_signup/main_app/views.py
+++ server/server_login_signup/main_app/views.py
@@ -7,7 +7,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
 from .models import Blog
 from .serializers import BlogSerializer
 from rest_framework import generics
@@ -91,7 +90,7 @@ class ProfileView(APIView):
 class BlogListCreateView(generics.ListCreateAPIView):
     queryset = Blog.objects.all().order_by('-created_at')
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated] # Logged in users can see and create
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -106,5 +105,4 @@ class BlogListCreateView(generics.ListCreateAPIView):
 class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
