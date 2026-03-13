8c220bb142d97c4c524f7fad0112ba7ee56eb696
surajacharya12 <surajacharya.080@kathford.edu.np>
2026-03-13 20:55:21 +0545

    blog image

--- server/server_login_signup/main_app/models.py
+++ server/server_login_signup/main_app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    topic = models.CharField(max_length=150)
+    topic = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
